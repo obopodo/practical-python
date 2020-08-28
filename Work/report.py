6347973329a77d349f32c9706e0349d2d4a066d4
Boris Prokis <bpodolnyi@gmail.com>
2020-08-28 14:24:17 +0300

    Stock class and TableFormatter class defined

--- Work/report.py
+++ Work/report.py
@@ -4,43 +4,78 @@
 import csv
 from pprint import pprint
 import fileparse as fp
+import stock
+import tableformat
 
 def read_prices(prices_filename):
     '''Function that reads prices set'''
     with open(prices_filename, 'rt') as inf:
         lines = fp.parse_csv(inf, has_headers = False, types=[str, float])
     prices = {name: price for name, price in lines}
-    # prices = fp.parse_csv(prices_filename, has_headers = False, types=[str, float])
     return prices
+
 
 def read_portfolio(portfolio_filename):
     '''
     Function that reads portfolio dataset into a list of dicts
     '''
     with open(portfolio_filename, 'rt') as inf:
-        portfolio = fp.parse_csv(inf, select=['name', 'shares', 'price'], types=[str, int, float])
+        portdicts = fp.parse_csv(inf, select=['name', 'shares', 'price'], types=[str, int, float])
+    portfolio = [stock.Stock(d['name'], d['shares'], d['price']) for d in portdicts]
     return portfolio
 
 
 def make_report(portfolio, prices):
     report = []
     for row in portfolio:
-        # report.append((row['name'], row['shares'], '$'+str(prices[row['name']]), prices[row['name']] - row['price']))
-        report.append((row['name'], row['shares'], prices[row['name']], prices[row['name']] - row['price']))
+        report.append((row.name, row.shares, prices[row.name], prices[row.name] - row.price))
     return report
 
-def print_report(report, header=('Name', 'Shares', 'Price', 'Change')):
-    print('%10s %10s %10s %10s' % header)
-    print('%10s %10s %10s %10s' % tuple(['-'*10]*4))
-    for r in report:
-        print(f'%10s %10d %10s %10.2f' % r)
+
+def print_report(report, formatter):
+    '''
+    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    '''
+    formatter.headings(['Name', 'Shares', 'Price', 'Change'])
+    for name, shares, price, change in report:
+        rowdata = [name, str(shares), f'{price:0.2f}', f'{change:0.2f}']
+        formatter.row(rowdata)
+
+
+def choose_formatter(format='txt'):
+    '''
+    Assign a type to formatter
+    '''
+    if format.lower() == 'txt':
+        formatter = tableformat.TextTableFormatter()
+    elif format.lower() == 'csv':
+        formatter = tableformat.CSVTableFormatter()
+    elif format.lower() == 'html':
+        formatter = tableformat.HTMLTableformatter()
+    else:
+        raise ValueError(f'Sorry, format \'{format}\' is not supported')
+    return formatter
+
+
+def portfolio_report(portfolio_filename, prices_filename, format='txt'):
+    '''
+    Make a stock report given portfolio and price data files.
+    '''
+    # Read data:
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    # Create report data:
+    report = make_report(portfolio, prices)
+    # Print it out:
+    formatter = choose_formatter(format)
+    print_report(report, formatter)
+
 
 def main(args:list):
-    portfolio = read_portfolio(args[1])
-    prices = read_prices(args[2])
-    report = make_report(portfolio, prices)
-    print_report(report)
-    # return portfolio, prices, report
+    if len(args) == 3:
+        portfolio_report(args[1], args[2])
+    elif len(args) == 4:
+        portfolio_report(args[1], args[2], args[3])
 
 
 if __name__ == '__main__':
